83f7148a94ca714f5aeb31ea8b2b2056cab06eb3
Steffano da Cruz <108758837+steffano-da-cruz@users.noreply.github.com>
2023-07-05 14:32:46 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-//Acquires the page elements
+//Acquire the html elements
 const frm = document.querySelector("form");
-//Indicates where the result is gonna be shown
 const result1 = document.querySelector("#outResult1");
 const result2 = document.querySelector("#outResult2");
 
@@ -11,16 +10,15 @@ let billsTotal = 0;
 
 frm.addEventListener("submit", (e) => {
   e.preventDefault(); //Avoid sending the form
-  const billDescription = frm.inBillDescription.value; //Acquires the name of the bill
-  const billValue = Number(frm.inBillValue.value); //Acquires the value of the bill
+  const billDescription = frm.inBillDescription.value; //Acquire the name of the bill
+  const billValue = Number(frm.inBillValue.value); //Acquire the value of the bill
 
   numBills++; //numBills = numBills + 1 (counter)
   billsTotal += billValue; //billsTotal = billsTotal + billValue (accumulator)
-  response =
-    response + billDescription + " - $: " + billValue.toFixed(2) + "\n";
+  response = response + billDescription + " - $" + billValue.toFixed(2) + "\n";
 
   result1.innerText = `${response}-----------------------------------`;
-  result2.innerText = `${numBills} Bill(s) - Total $: ${billsTotal.toFixed(2)}`;
+  result2.innerText = `${numBills} Bill(s) - Total: $${billsTotal.toFixed(2)}`;
 
   frm.inBillDescription.value = ""; //Cleans the bill description field
   frm.inBillValue.value = ""; //Cleans the bill value field
